e373bb9a82d6b969d03907e3094fc3bde4ec94e1
Palloma Gualter <pallomagualter@gmail.com>
2020-10-14 17:02:52 -0300

    post

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Feed from './pages/Feed';
+import New from './pages/New';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+
+          <AppStack.Navigator screenOptions={{ headerShown: false}}>
+            <AppStack.Screen name="Feed" component={Feed} />
+            <AppStack.Screen name="New" component={New} />
+          </AppStack.Navigator>
+        </NavigationContainer>
+    )
+
+}
+
+/*
+ createStackNavigator é para navegação em pilha, enquanto uma tela está ativa existe várias ativas ao mesmo tempo.
+ createSwitchNavigator só uma tela ficará tiva por vez, o usuário não poderá voltar para tela anterior.
+ */
