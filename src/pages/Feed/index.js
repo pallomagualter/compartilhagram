ad131f54cf781641208fb3751b98a6cd93500d5a
Palloma Gualter <pallomagualter@gmail.com>
2020-10-16 11:20:17 -0300

    up

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,1 +1,17 @@
-impo
+import React from 'react';
+import { View, Image} from 'react-native';
+
+import logo from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function Feed () {
+        return(
+                <View style={styles.container}> 
+                        <View >
+                                <Image style={styles.feedImage} source={logo} />
+                        </View>
+                </View>
+        ) 
+}
+
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-const AppStack = createStackNavigator();
 
 import Feed from './pages/Feed';
 import New from './pages/New';
 
+const AppStack = createStackNavigator();
 
 export default function Routes(){
   return(
